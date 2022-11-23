3c7835a7e83aba1ab38be6b050f679430084b2e8
Eri França <87365333+EriFranca@users.noreply.github.com>
2022-11-23 06:58:08 -0300

    kata 05

---
+++ JAVASCRIPT/KATA 05/RGB To Hex Conversion/solução.js
@@ -0,0 +1,6 @@
+const rgb = (...arg) => arg
+  .map(it => Math.max(Math.min(it, 255), 0)
+    .toString(16)
+    .padStart(2, '0')
+    .toUpperCase()
+   ).join('');
---
+++ JAVASCRIPT/KATA 05/Valid Parentheses/solução.js
@@ -0,0 +1,9 @@
+function validParentheses(parens){
+    
+    const reg = /\(\)/g;
+    while(reg.test(parens)) {
+      parens = parens.replace(reg, '');
+    }
+      
+    return parens === '';
+  }
