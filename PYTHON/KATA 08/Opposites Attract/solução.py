b53c742d3713573bc8b7325554e63f8c82f6600a
Eri França <87365333+EriFranca@users.noreply.github.com>
2022-11-23 12:12:37 -0300

    python kata 08

---
+++ PYTHON/KATA 08/Opposites Attract/solução.py
@@ -0,0 +1,7 @@
+def lovefunc(flower1, flower2):
+    """Return true if one number is odd and the other is not."""
+    if not flower1 % 2 and flower2 % 2:
+        return True
+    elif not flower2 % 2 and flower1 % 2:
+        return True
+    return False
